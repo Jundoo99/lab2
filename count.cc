acefe66fe088262ac4300d7624526e8e11304045
Jundoo99 <junedanny@naver.com>
2022-09-11 16:19:42 +0900

    add count.cc

---
+++ count.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void count_(int num){
+	
+	for(int i=1;i<=num;i++)
+		cout << i << endl;
+
+	cout << "Start!!" << endl << "2018310695 이준석" << endl;
+}
